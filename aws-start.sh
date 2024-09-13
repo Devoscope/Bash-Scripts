90537c2d3d61e890381e3363dd43d4013ceb2ccd
sairaj1994 <95277689+sairaj1994@users.noreply.github.com>
2024-09-13 21:32:06 +0530

    Update aws-start.sh

--- aws-start.sh
+++ aws-start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Starting QAT-prod jumpoint "
 date
-aws ec2 start-instances --instance-ids i-0c1928487b86ece18
+aws ec2 start-instances --instance-ids i-0c1928487b86xxxx
 sleep 5;
-echo "Starting QAT-prod jumpoint."
+echo "Starting instance i-0c1928487b86xxxx."
