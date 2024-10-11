46628a332c08b89960faec9b0bab1b89f6ddcc19
sairaj1994 <95277689+sairaj1994@users.noreply.github.com>
2024-10-11 17:31:45 +0530

    modified the script

--- aws_input.sh
+++ aws_input.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
+echo "#####################################  WARNING  #####################################"
+echo "You are about to modify the instance."
+echo
 instance_id="i-0c1928487b8xxxx"
+
+VAR_SMALL_Y="y"
+VAR_CAP_Y="Y"
+VAR_SMALL_N="n"
+VAR_CAP_N="N"
 echo "Hey user, this script will start/stop your $instance_id"
-# Ask the user for their input
 echo "Do you want to start or stop the script (enter start/stop)?"
 read varname
-#echo "You chose to $varname the instance. Please confirm (yes/no)?"
-#read input
-#if $input == "yes"; then
-aws ec2 $varname-instances --instance-ids $instance_id
-echo "Starting instance $instance_id."
-#else
-#echo Please try again.
-#fi
+read -p "Are you sure you want to proceed? (Y/N)" RESPONSE
+if [ $RESPONSE = 'Y' ] || [ $RESPONSE = 'y' ] || [ $RESPONSE = 'N' ] || [ $RESPONSE = 'n' ]
+then
+        if [ $RESPONSE = 'Y' ] || [ $RESPONSE = 'y' ]
+        then
+                echo "$varname instance"
+                aws ec2 $varname-instances --instance-ids $instance_id
+                echo "Action completed"
+        else 
+                echo "Restore aborted based on input"
+        fi
+else
+        echo "Incorrect input received"
+fi
